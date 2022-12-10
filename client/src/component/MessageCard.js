52a1076e71acb60a081755e3441107a163a2e942
marycatau <marycatherine.au@gmail.com>
2022-12-10 16:46:01 +0000

    completed the Message function

--- client/src/component/MessageCard.js
+++ client/src/component/MessageCard.js
@@ -1,44 +1,103 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react';
+import {CHANGE_ITEM_OWNER, UPDATE_MESSAGE} from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 export default function MessageCard(props) {
     const {swapData} = props;
+    const [changeItemOwner] = useMutation(CHANGE_ITEM_OWNER);
+    const [updateMessage] = useMutation(UPDATE_MESSAGE);
+
     console.log(swapData);
 
-        return (
-            <Card>
-              <Card.Content>
-                <Image
-                  floated='right'
-                  size='mini'
-                  src={swapData.itemRequest.image}
-                />
-                <Card.Header>Swap Request</Card.Header>
-                <Card.Meta></Card.Meta>
-                <Card.Description>
-                    <p>Your Product: {swapData.itemRequest.name}</p>
-                    <p>Swap Request from: {swapData.sender.name}</p>
-                    <p>Swap to Product: {swapData.itemOffer.name}</p>
-                    <p>Product Description:{swapData.itemOffer.description}</p>
-                </Card.Description>
-                <Image
-                  floated='right'
-                  size='mini'
-                  src={ swapData.itemOffer.image}
-                />
-                    <p></p>
-              </Card.Content>
-              <Card.Content extra>
-                <div className='ui two buttons'>
-                  <Button basic color='green' >
-                    Approve
-                  </Button>
-                  <Button basic color='red' >
-                    Decline 
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
+    //handle submit
+    const handleApprove = async (event) => {
+      event.preventDefault();
+      //mutation change owner x 2      
+      try {
+        const mutationResponse = await changeItemOwner({
+          variables:{_id: swapData.itemRequest._id, owner: swapData.sender._id}
+        });
+        const mutationResponse2 = await changeItemOwner({
+          variables:{_id: swapData.itemOffer._id, owner: swapData.receiver._id}
+        });
+        const mutationResponse3 = await updateMessage({
+          variables:{_id: swapData._id, isAgree:true ,isClosed:true,replyMessage:""}    
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      window.location.assign('/DashBoard');
 
-        )
+    }
+
+    const handleDecline = async (event) => {
+      event.preventDefault();
+      //updateMessage(_id: $_id, isAgree: $isAgree, isClosed: $isClosed, replyMessage: $replyMessage )
+      try {
+        const mutationResponse4 = await updateMessage({
+          variables:{_id: swapData._id, isAgree:false ,isClosed:true,replyMessage:""}
+        });
+        console.log(mutationResponse4);        
+        console.log('message sent');
+        alert('message sent');             
+      } catch (e) {
+        console.log(e);
+      }
+      window.location.assign('/DashBoard');
+    }
+
+
+    
+
+
+    var approvalPanel = (
+      <div className='ui two buttons'>
+        <Button basic color='green' onClick={handleApprove}>
+          Approve
+        </Button>
+        <Button basic color='red' onClick={handleDecline}>
+          Decline 
+        </Button>
+      </div>
+    );
+  
+    
+    if(swapData && swapData.isClosed){
+      approvalPanel = swapData.isAgree?(
+        <div>This swap request is approved</div>
+      ):(
+        <div>This swap request is declined.</div>
+      );
+    }
+    
+
+    return (
+        <Card>
+          <Card.Content>
+            <Image
+              floated='right'
+              size='mini'
+              src={swapData.itemRequest.image}
+            />
+            <Card.Header>Swap Request</Card.Header>
+            <Card.Meta></Card.Meta>
+            <Card.Description>
+                <p>Your Product: {swapData.itemRequest.name}</p>
+                <p>Swap Request from: {swapData.sender.name}</p>
+                <p>Swap to Product: {swapData.itemOffer.name}</p>
+                <p>Product Description:{swapData.itemOffer.description}</p>
+            </Card.Description>
+            <Image
+              floated='right'
+              size='mini'
+              src={ swapData.itemOffer.image}
+            />
+          </Card.Content>
+          <Card.Content extra>
+            {approvalPanel}
+          </Card.Content>
+        </Card>
+
+    )
 }
